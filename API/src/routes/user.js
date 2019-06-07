c3a6b61cdd8ced717dabf9448f7113198d99a953
salviosage <salviosage@gmail.com>
2019-06-07 17:02:17 +0200

    feature(passwordreset) reseting password endpoint
    [Finishes #166267530]

--- API/src/routes/user.js
+++ API/src/routes/user.js
@@ -5,6 +5,7 @@ const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup); 
 router.post('/login', userCtrl.login);
+router.patch('/reset', userCtrl.reset);
  
 router.get('/users', userCtrl.getUser);
 
