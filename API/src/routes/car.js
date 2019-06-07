92c754fcf8bf652e693bcb5a49740bfb0bf11e50
salviosage <salviosage@gmail.com>
2019-06-07 15:54:20 +0200

    feature(delete) add ads deletion endpoint
    admin can delete a ny car
    
    [Finishes #166267516]

--- API/src/routes/car.js
+++ API/src/routes/car.js
@@ -12,7 +12,7 @@ router.post('/', carCtrl.createAd );
 router.get('/', carCtrl.getAds );
 router.get('/:id', carCtrl.getOneAd );
 //router.put('/:id', auth, function(req, res){carCtrl.modifycar});
-//router.delete('/:id', auth,function(req, res){ carCtrl.deleteAd});
+router.delete('/:id',carCtrl.deleteAd);
 //router.patch('/:id/status', auth,function(req, res){ carCtrl.updateAdStatus});
 router.patch('/:id/price', carCtrl.updateAd);
 
